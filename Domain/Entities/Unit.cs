900e832af99aec8c14e6fe199c0644f23fc25f6a
Avi-Karalis <averkkaralis@gmail.com>
2025-04-11 15:30:31 +0300

    connection to db completed

--- Domain/Entities/Unit.cs
+++ Domain/Entities/Unit.cs
@@ -8,6 +8,8 @@ namespace Domain.Entities {
         public required string Faction { get; set; }
         public required string UnitType { get; set; }
         public required IEnumerable<string> Designation { get; set; }
+        public string? DesignationTypeLimit { get; set; }
+        public sbyte DesignationLimitValue { get; set; } = 0
         public ICollection<UnitSpecialAbility>? UnitSpecialAbilities { get; set; }
         public required sbyte DPCost { get; set; }
         public required sbyte SPCost { get; set; }

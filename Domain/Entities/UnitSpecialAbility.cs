577ae32f78c9decc1a3c2484b7b9fc204f62a2d9
Avi-Karalis <averkkaralis@gmail.com>
2025-04-11 15:16:15 +0300

    connection to db completed

--- Domain/Entities/UnitSpecialAbility.cs
+++ Domain/Entities/UnitSpecialAbility.cs
@@ -6,7 +6,8 @@ namespace Domain.Entities {
     [Table("UnitSpecialAbilities")]
     public class UnitSpecialAbility : BaseEntity {
         public required string Name { get; set; }
-        public sbyte? NumericValue { get; set; }
+        public string? NumericValueX { get; set; }
+        public string? NumericValueY { get; set; }
         public required string Description { get; set; }
     }
 }
